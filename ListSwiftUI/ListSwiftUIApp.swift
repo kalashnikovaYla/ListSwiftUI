e53cf041465abc3051653c236571db93946142ca
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-10 23:45:03 +0500

    Initial Commit

---
+++ ListSwiftUI/ListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSwiftUIApp.swift
+//  ListSwiftUI
+//
+//  Created by sss on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
